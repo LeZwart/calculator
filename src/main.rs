a3196d4fe9dc133d39fbb09d0abd9db09faf7e27
LeZwart <192443@novacollege.nl>
2024-11-29 10:38:49 +0100

    Add regex dependency, calculate equation, check for invalid characters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use regex::Regex;
 
 fn readline(prompt: &str) -> String {
     println!("{}", prompt);
@@ -9,24 +10,69 @@ fn readline(prompt: &str) -> String {
     return input.trim().to_string();
 }
 
+fn is_operator(c: char) -> bool {
+    match c {
+        '+' | '-' | '*' | '/' => true,
+        _ => false,
+    }
+}
 
+fn has_invalid_character(equation_parts: &Vec<&str>) -> bool {
+    for part in equation_parts {
+        if part.parse::<f32>().is_err() {
+            if !is_operator(part.chars().next().unwrap()) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 fn main() {
     loop {
-        let input1: String = readline("First value: ");
-        let input2: String = readline("Second value: ");
-
-        if input1.parse::<f32>().is_ok() && input2.parse::<f32>().is_ok() {
-            let value1: f32 = input1.parse().expect("Failed to parse input1");
-            let value2: f32 = input2.parse().expect("Failed to parse input2");
-
-            let result: f32 = value1 + value2;
-
-            println!("Result is: {}", result);
-
-        } else {
-            println!("Invalid characters in use!");
+        let input: String = readline("Calculate:");
+        if input.is_empty() {
+            return;
         }
+        
+        let re = Regex::new(r"(\d+|\D)").unwrap();
+        let parts: Vec<&str> = re.find_iter(&input).map(|m| m.as_str()).collect();
+        
+        // Borrow 'parts' to check if the equation contains invalid characters
+        if has_invalid_character(&parts) {
+            println!("Invalid characters in equation");
+            continue;
+        }
+
+        // println!("{:?}", parts);
+        
+        // ["25", "+", "2", "+", "7", "-", "2"]
+        // Initialize the result with the first number
+        let mut result: f32 = parts[0].parse().unwrap();
+
+        for i in 1..parts.len() {
+            let part = parts[i];
+
+            // Is current part is an operator
+            if is_operator(part.chars().next().unwrap()) {    
+                let operator = part.chars().next().unwrap();
+
+                // Parse the next part as a number (operand)
+                let operand = parts[i + 1].parse::<f32>().unwrap();
+
+                // Perform the operation based on the operator
+                match operator {
+                    '+' => result += operand,
+                    '-' => result -= operand,
+                    '*' => result *= operand,
+                    '/' => result /= operand,
+                    _ => (),
+                }
+            }
+        }
+
+        println!("{}", result);
+        
 
     }
 
